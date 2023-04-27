8936c85cf2cde193eb09671ec9c54cb1d976b605
AnPhatDat2 <130839954+AnPhatDat2@users.noreply.github.com>
2023-04-27 18:32:39 +0700

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,8 @@
+local DarkraiX = loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Kavo-Ui/main/Darkrai%20Ui", true))()
+
+local Library = DarkraiX:Window("Cu X Premium","","",Enum.KeyCode.RightControl);
+
+TabMain = Library:Tab("Main")
+TabMain:Button("Mukuro Open",function()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/BF")()
+        end)
